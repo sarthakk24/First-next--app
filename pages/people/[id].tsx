d64d1b6f1fe9ea3d53f122fa0aa055933aec25f3
sarthak sachdeva <sarthak.sachdeva.73@gmail.com>
2022-02-09 04:24:40 +0530

    :construction: added styles for per page using css
    added styles

--- pages/people/[id].tsx
+++ pages/people/[id].tsx
@@ -1,24 +1,52 @@
 import Head from "next/head";
 import Link from "next/link";
 import Avatar from "react-avatar";
+import PersonData from "../../components/People/PersonData";
 import { person } from "../../interface";
 import styles from "../../styles/people/peoplePersonal.module.css";
 
 const Render = (props: { person: person }) => {
+  const { id, name, username, email, address, phone, website, company } =
+    props.person;
+
+  const addressFinal: string = `${address.street}, ${address.suite}, ${address.city}, ${address.zipcode} `;
+
   return (
-    <div>
+    <>
       <Head>
         <title>{props.person.name}</title>
       </Head>
 
-      <div>
+      {/* <div>
         <Link href={"/people"}>Back</Link>
         <Link href={"/"}>Home</Link>
-      </div>
+      </div> */}
       <div className={`${styles.mainContainer}`}>
-        <Avatar name={props.person.name} className={styles.avatar} />
+        <div className={styles.container}>
+          <div className={styles.introContainer}>
+            <Avatar name={name} className={styles.avatar} />
+            <div>
+              <PersonData label="Username" value={username} />
+              <PersonData label="Full Name" value={name} />
+              <PersonData label="Email" value={email} />
+            </div>
+          </div>
+          <div className={styles.companyContainer}>
+            <Avatar name={company.name} className={styles.companyAvatar} />
+            <div>
+              <PersonData label="Company Name" value={company.name} />
+              <PersonData label="Website" value={website} />
+              <PersonData label="catchphrase" value={company.catchPhrase} />
+              <PersonData label="Company Name" value={company.bs} />
+            </div>
+          </div>
+          <div className={styles.addressContainer}>
+            <PersonData label="Address" value={addressFinal} />
+            <PersonData label="Phone" value={phone} />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
