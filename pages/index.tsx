39cbbc7501c87d7fb8c41c77e0e64e49dfe579b7
sarthak sachdeva <sarthak.sachdeva.73@gmail.com>
2022-02-09 22:58:27 +0530

    :iphone: responsive design
    worked on responsive design for several pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,11 @@
 import type { NextPage } from "next";
-import Link from "next/link";
 import Navbar from "../components/Navbar/Navbar";
 import styles from "../styles/home/Home.module.css";
-import humans from "../components/lottie/background.jpg";
-import Image from "next/image";
+import buttonStyles from "../styles/UI/buttons.module.css";
 import NavbarItem from "../components/Navbar/NavbarItem";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  // <div className={styles.contentContainer}>
-  //   <p>Get detailed data on 10 random humans on planet earth.</p>
-  //   <Link href="/people">
-  //     <button>People</button>
-  //   </Link>
-  // </div>
-  // <div className={`${styles.imageContainer}`}>
-  //   {/* <img
-  //     src="../components/lottie/background.jpg"
-  //     alt="Humans"
-  //     className={`${styles.humanImg}`}
-  //   /> */}
-  //   <Image src={humans} alt="Humans" className={`${styles.humanImg}`} />
-  // </div>
   return (
     <div className={styles.home}>
       <Navbar>
@@ -31,9 +16,12 @@ const Home: NextPage = () => {
           <NavbarItem href="/people/1" name="Example" />
         </div>
       </Navbar>
-      <div>
-        <div className={`${styles.imageContainer}`}>
-          <Image src={humans} alt="human" />
+      <div className={styles.humanContainer}>
+        <div>This website provides dataBase for random humans</div>
+        <div className={styles.btn}>
+          <Link href="/people">
+            <a className={buttonStyles.button}>People</a>
+          </Link>
         </div>
       </div>
     </div>
